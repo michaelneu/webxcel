c3e541405f0ea130523a695aebcaa790e30d23a1
Michael Neu <git@michaeln.eu>
2019-10-01 22:34:48 +0100

    add export script

---
+++ export.ps1
@@ -0,0 +1,37 @@
+. .\variables.ps1
+. .\constants.ps1
+. .\log.ps1
+
+LogInfo "Collecting modules"
+
+$missing = [System.Reflection.Missing]::Value
+$excel = New-Object -ComObject Excel.Application
+$document = $excel.Workbooks.Open($FILENAME, $missing, $true)
+$modules = $document.VBProject.VBComponents;
+$exportedModules = 0
+
+For ($moduleIndex = 0; $moduleIndex -lt $modules.Count; $moduleIndex++)
+{
+    $module = $modules.Item($moduleIndex + 1)
+    $moduleFilename = switch ($module.Type)
+    {
+        $COMPONENT_TYPE_MODULE { "src\Modules\$($module.Name).bas" }
+        $COMPONENT_TYPE_CLASS { "src\Classes\$($module.Name).cls" }
+        default { "" }
+    }
+
+    if ($moduleFilename -eq "")
+    {
+        echo "skipping module '$($module.Name)'"
+        continue
+    }
+
+    $moduleDestination = [IO.Path]::Combine($CWD, $moduleFilename)
+    echo "exporting $moduleFilename"
+    $module.Export($moduleDestination)
+    $exportedModules += 1
+}
+
+$excel.Quit()
+LogInfo "Exported $exportedModules modules"
+LogEmptyLine
